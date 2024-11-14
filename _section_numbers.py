6c8fee9d58a4836c2c226b3c73947dfa7250a9cc
jtvandijk <j.t.vandijk@ucl.ac.uk>
2024-11-14 09:38:24 +0000

    fix toc numbering installation page

--- _section_numbers.py
+++ _section_numbers.py
@@ -53,6 +53,33 @@ for htmlf in html_files:
     # write
     with open(htmlf, 'w') as file:
         file.write(html_content)
+        
+# fix installation page
+htmlinstall = 'docs/00-installation.html'
+
+with open(htmlinstall) as file:
+    html_content = file.read()
+    
+    # update TOC chapter 1
+    html_content = html_content.replace('<span class="menu-text">R for Data Analysis</span>', \
+                                        '<span class="menu-text">1 R for Data Analysis</span>')
+    # update TOC chapter 2
+    html_content = html_content.replace('<span class="menu-text">Statistical Analysis I</span>', \
+                                        '<span class="menu-text">2 Statistical Analysis I</span>')
+    # update TOC chapter 3
+    html_content = html_content.replace('<span class="menu-text">Statistical Analysis II</span>', \
+                                        '<span class="menu-text">3 Statistical Analysis II</span>')
+    # update TOC chapter 4
+    html_content = html_content.replace('<span class="menu-text">Spatial Analysis I</span>', \
+                                        '<span class="menu-text">4 Spatial Analysis I</span>')
+    
+    # update TOC chapter 5
+    html_content = html_content.replace('<span class="menu-text">Spatial Analysis II</span>', \
+                                        '<span class="menu-text">5 Spatial Analysis II</span>')
+                                        
+    # write
+    with open (htmlinstall, 'w') as file:
+      file.write(html_content)              
 
 # fix index page redirect
 htmlredir = 'docs/index.html'
